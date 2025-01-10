30c09682d25f670ab085c1d47a216225ece17858
rgupta00 <rgupta.mtech@gmail.com>
2025-01-10 12:42:44 +0530

    day 4 first half

---
+++ week1/day-4-io-ex/src/com/day4/session2/E_Ex_Overriding.java
@@ -0,0 +1,7 @@
+package com.day4.session2;
+
+public class E_Ex_Overriding {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week1/day-4-io-ex/src/com/day4/session2/F_Ex_Ctr.java
@@ -0,0 +1,7 @@
+package com.day4.session2;
+
+public class F_Ex_Ctr {
+    public static void main(String[] args) {
+
+    }
+}
