3ca2d7dc14f21e14235ccff42ccc9f94d30fbdf0
rgupta00 <rgupta.mtech@gmail.com>
2025-01-29 12:55:37 +0530

    spring boot logging

--- week3/spring_boot/bookapp01/src/main/java/com/bookapp/Bookapp01Application.java
+++ week3/spring_boot/bookapp01/src/main/java/com/bookapp/Bookapp01Application.java
@@ -22,7 +22,7 @@ public class Bookapp01Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		bookRepo.save(new Book("123", "Rich dad poor dad", "LifeSkill", "RK", BigDecimal.valueOf(200.00)));
-		bookRepo.save(new Book("456", "The Alchemist", "LifeSkill", "foo", BigDecimal.valueOf(300.00)));
+//		bookRepo.save(new Book("123", "Rich dad poor dad", "LifeSkill", "RK", BigDecimal.valueOf(200.00)));
+//		bookRepo.save(new Book("456", "The Alchemist", "LifeSkill", "foo", BigDecimal.valueOf(300.00)));
 	}
 }
---
+++ week3/spring_boot_again/productapp/src/main/java/com/productapp/ProductappApplication.java
@@ -0,0 +1,33 @@
+package com.productapp;
+
+import com.productapp.dto.InfoDto;
+import com.productapp.repo.Product;
+import com.productapp.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.math.BigDecimal;
+
+//CommandLineRunner
+
+@SpringBootApplication
+@EnableConfigurationProperties(InfoDto.class)
+public class ProductappApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepo productRepo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		productRepo.save(new Product("laptop", BigDecimal.valueOf(50000.00)));
+//		productRepo.save(new Product("mobile", BigDecimal.valueOf(5000.00)));
+	//	System.out.println("-----------added----------------");
+	}
+}
