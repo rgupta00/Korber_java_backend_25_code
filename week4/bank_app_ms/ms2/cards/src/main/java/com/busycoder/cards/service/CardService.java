64f94a083f0d7edb44cb1f472249b1699c73d491
rgupta00 <rgupta.mtech@gmail.com>
2025-01-31 13:56:30 +0530

    config server

--- week4/bank_app_ms/ms2/cards/src/main/java/com/busycoder/cards/service/CardService.java
+++
@@ -1,7 +0,0 @@
-package com.busycoder.cards.service;
-
-import com.busycoder.cards.dto.CardDto;
-
-public interface CardService {
-   public CardDto findByMobileNumber(String mobile);
-}
--- week4/bank_app_ms/ms2/loans/src/main/java/com/busycoder/loans/services/LoanService.java
+++
@@ -1,8 +0,0 @@
-package com.busycoder.loans.services;
-
-import com.busycoder.loans.dto.LoanDto;
-import com.busycoder.loans.entities.Loan;
-
-public interface LoanService {
-    public LoanDto findByMobile(String mobile);
-}
