a9319e0033b2bc177d5d47f2b93edd891f86dbad
rgupta00 <rgupta.mtech@gmail.com>
2025-02-11 17:15:50 +0530

    config server ex is done

--- week6/config_server_example/app1/src/main/java/com/app1/controller/App1Controller.java
+++ week6/config_server_example/app1/src/main/java/com/app1/controller/App1Controller.java
@@ -1,10 +1,12 @@
 package com.app1.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class App1Controller {
     @Value("${app.name}")
     private String val;
@@ -13,4 +15,5 @@ public class App1Controller {
     public String hello(){
         return "hello app1 : "+val;
     }
+
 }
--- week6/config_server_example/app2/src/main/java/com/app2/controller/App2Controller.java
+++ week6/config_server_example/app2/src/main/java/com/app2/controller/App2Controller.java
@@ -1,10 +1,12 @@
 package com.app2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class App2Controller {
     @Value("${app.name}")
     private String val;
